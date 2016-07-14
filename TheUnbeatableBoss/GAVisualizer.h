888dd24057916a80cc6328ac3ae26075e7d2e6d0
ash9991win <ash9991win@gmail.com>
2016-07-14 14:32:59 -0400

    Created legend table for displaying what each encoded gene stands for. Cached label texts for optimization

--- TheUnbeatableBoss/GAVisualizer.h
+++ TheUnbeatableBoss/GAVisualizer.h
@@ -17,18 +17,45 @@ class GAVisualizer
 	vector<Genome<T>> Genomes;
 	std::mutex mGALock;
 	map<int, string> stringTable;
+	sf::Text *LegendTable;
+	int rowLegendCount;
+	sf::Text generationText;
+	sf::Text ChromosomeLabel;
+	sf::Text Fitnesslabel;
+	int MaxFitnessIndex;
 public:
 	void AssignGA(GeneticAlgorithm<T>& ga)
 	{
 		mGA = &ga;
 		mGA->EpochDelegate.Bind( &GAVisualizer<T>::UpdateGeneration,this);
 		stringTable = Genome<T>::StringDecoder;
+	
+		
+	}
+	void PopulateLegends()
+	{
+		rowLegendCount = stringTable.size();
+		LegendTable = new sf::Text[rowLegendCount];
+		sf::Vector2f startingPositionOfTable;
+		startingPositionOfTable.x = windowWidth / 2 + 25;
+		startingPositionOfTable.y = windowHeight / 2;
+		for (int i = 0; i < rowLegendCount; i++)
+		{
+			ostringstream stream;
+			stream << i << "     " << stringTable[i];
+			LegendTable[i].setString(stream.str());
+			LegendTable[i].setColor(sf::Color::Green);
+			LegendTable[i].setFont(*(ResourceManager::GetGameFont()));
+			LegendTable[i].setPosition(startingPositionOfTable);
+			startingPositionOfTable.y += 50;
+		}
 	}
 	 void UpdateGeneration()
 	{
 		{
 			std::lock_guard<mutex> g(mGALock);
 				Genomes = mGA->GetAllGenomes();
+				MaxFitnessIndex = mGA->GetFittest();
 		}
 	}
 
@@ -37,12 +64,23 @@ public:
 		mWindow = &window;
 		windowWidth = mWindow->getSize().x;
 		windowHeight = mWindow->getSize().y;
+
+		
+
+		ChromosomeLabel.setString("CHROMOSOME");
+		ChromosomeLabel.setPosition(0, 10);
+		ChromosomeLabel.setColor(sf::Color::Black);
+		ChromosomeLabel.setFont(*(ResourceManager::GetGameFont()));
+		Fitnesslabel.setString("FITNESS");
+		Fitnesslabel.setPosition(320, 10);
+		Fitnesslabel.setColor(sf::Color::Black);
+		Fitnesslabel.setFont(*(ResourceManager::GetGameFont()));
+		
 	}
 
 	void Render()
 	{
 		//First display the generation number
-		sf::Text generationText;
 		ostringstream generationString;
 		generationString << "Generation Number:    " << mGA->Generation();
 		generationText.setString(generationString.str());
@@ -51,19 +89,10 @@ public:
 		generationText.setFont(*(ResourceManager::GetGameFont()));
 		mWindow->draw(generationText);
 		//Display the first child as a table
-		sf::Text ChromosomeLabel;
-		ChromosomeLabel.setString("CHROMOSOME");
-		ChromosomeLabel.setPosition(0, 10);
-		ChromosomeLabel.setColor(sf::Color::Black);
-		ChromosomeLabel.setFont(*(ResourceManager::GetGameFont()));
-		sf::Text Fitnesslabel;
-		Fitnesslabel.setString("FITNESS");
-		Fitnesslabel.setPosition(320, 10);
-		Fitnesslabel.setColor(sf::Color::Black);
-		Fitnesslabel.setFont(*(ResourceManager::GetGameFont()));
-
-
-
+		for (int i = 0; i < rowLegendCount; i++)
+		{
+			mWindow->draw(LegendTable[i]);
+		}
 		mWindow->draw(ChromosomeLabel);
 		mWindow->draw(Fitnesslabel);
 		{
@@ -81,7 +110,14 @@ public:
 					chromosomeText.setString(chromosomeStream.str());
 					chromosomeText.setFont(*(ResourceManager::GetGameFont()));
 					chromosomeText.setPosition(0,50 + (i * 30 ));
-					chromosomeText.setColor(sf::Color::Black);
+					if (i == MaxFitnessIndex)
+					{
+						chromosomeText.setColor(sf::Color::Red);
+					}
+					else
+					{
+						chromosomeText.setColor(sf::Color::Black);
+					}
 					//chromosomeText.setScale(0.45f, 0.45f);
 
 					mWindow->draw(chromosomeText);
